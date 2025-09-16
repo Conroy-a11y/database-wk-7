9876276f4ca5a25db5b53cbaaa3c72d59a8fd050
Conroy Makhubu <cmakhubu@gmail.com>
2025-09-16 20:15:30 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,5 +1,21 @@
 --Question1
--- Transform ProductDetail into 1NF
+
 SELECT OrderID, CustomerName, LTRIM(RTRIM(value)) AS Product
 FROM
     ProductDetail CROSS APPLY STRING_SPLIT (Products, ',');
+--Question2
+
+
+SELECT DISTINCT
+    OrderID,
+    CustomerName
+INTO Orders
+FROM OrderDetails;
+
+
+SELECT 
+    OrderID,
+    Product,
+    Quantity
+INTO OrderDetails_2NF
+FROM OrderDetails;
